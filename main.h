1f3e04ae7a7b6bc293f5e6d10011b7c73092ed22
root <yahiyamohammed455@gmail.com>
2022-04-19 12:14:17 -0700

    Fix main

--- main.h
+++ main.h
@@ -3,60 +3,34 @@
 
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdlib.h>
+
 /**
- * struct specifier - struct specifier.
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
  *
- * @valid: The valid character.
- * @f: The functions associated.
+ * Return: int
  */
-
-typedef struct specifier
+typedef struct structprint
 {
-	char *valid;
-	int (*f)(va_list);
-} spec;
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
 
-/* writes func */
-int _putchar(char c);
-
-/* converter */
-char *convert(unsigned long int num, int base, int lowercase);
-
-/* gets the specifier */
-int (*get_func(char x))(va_list args);
-
-/* prints the main specifier of all */
-int _printf(const char *format, ...);
-
-/* prints each specifier */
-int print_c(va_list args);
-int print_s(va_list args);
-int print_d(va_list args);
-int print_i(va_list args);
-int print_b(va_list args);
-int print_u(va_list args);
-int print_o(va_list args);
-int print_p(va_list args);
-int print_x(va_list args);
-int print_X(va_list args);
-
-/* prints percent '%' */
-int print_percent(va_list args);
-
-/* prints in reverse order */
-int print_rev(va_list args);
-int print_R(va_list args);
-
-/* prints the address of pointer */
-void print_pointer(unsigned long ptr, int *count);
-
-/* count a num system */
-int countDigits(unsigned int num);
-int countOctal(unsigned int num);
-int countBinary(unsigned int num);
-int print_number(unsigned int n);
-
-/* prints the length of a string */
-int _strlen(char *);
-
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
 #endif
